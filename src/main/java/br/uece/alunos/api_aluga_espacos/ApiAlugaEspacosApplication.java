b907d86b2c6b43de55edb28620c27e4d6239c530
André Lúcio Rocha Wanderley <andre.rocha@techne.com.br>
2025-06-05 15:03:47 -0300

    build inicial do projeto

---
+++ src/main/java/br/uece/alunos/api_aluga_espacos/ApiAlugaEspacosApplication.java
@@ -0,0 +1,13 @@
+package br.uece.alunos.api_aluga_espacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiAlugaEspacosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiAlugaEspacosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uece/alunos/api_aluga_espacos/ApiAlugaEspacosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.alunos.api_aluga_espacos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiAlugaEspacosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
