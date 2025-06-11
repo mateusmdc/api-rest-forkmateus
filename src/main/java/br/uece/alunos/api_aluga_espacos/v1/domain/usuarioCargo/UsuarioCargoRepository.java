c58073652fa37b70febd32fa6f8a210976cd88c2
Andre Lucio Rocha <andrerocha0911@gmail.com>
2025-06-11 15:41:37 -0300

    feat: mapeado usuariocurso e populate

--- src/main/java/br/uece/alunos/api_aluga_espacos/v1/domain/usuarioCargo/UsuarioCargoRepository.java
+++ src/main/java/br/uece/alunos/api_aluga_espacos/v1/domain/usuarioCargo/UsuarioCargoRepository.java
@@ -3,8 +3,6 @@ package br.uece.alunos.api_aluga_espacos.v1.domain.usuarioCargo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-import java.util.List;
-
 public interface UsuarioCargoRepository extends JpaRepository<UsuarioCargo, String> {
     @Query("SELECT uc FROM UsuarioCargo uc WHERE uc.usuario.id = :usuarioId")
     UsuarioCargo findByUsuarioId(String usuarioId);
