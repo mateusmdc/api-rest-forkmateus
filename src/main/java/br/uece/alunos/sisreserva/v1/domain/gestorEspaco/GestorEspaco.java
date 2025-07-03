6073d40206daaf8ddc8a8c1fbfa54c340376922c
Andre Lucio Rocha <andrerocha0911@gmail.com>
2025-07-03 14:30:25 -0300

    feat: mapeando gestor espaçõ

---
+++ src/main/java/br/uece/alunos/sisreserva/v1/domain/gestorEspaco/GestorEspaco.java
@@ -0,0 +1,55 @@
+package br.uece.alunos.sisreserva.v1.domain.gestorEspaco;
+
+import br.uece.alunos.sisreserva.v1.domain.espaco.Espaco;
+import br.uece.alunos.sisreserva.v1.domain.usuario.Usuario;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Table(name = "gestor_espaco")
+@Entity(name = "GestorEspaco")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class GestorEspaco {
+    @Id
+    @Column(name = "id", nullable = false, length = 36, updatable = false)
+    private String id;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "usuario_gestor_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Usuario usuarioGestor;
+
+    @NotNull
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "espaco_id", referencedColumnName = "id", nullable = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    private Espaco espaco;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    public void onCreate() {
+        this.id = UUID.randomUUID().toString().toUpperCase();
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
+}
