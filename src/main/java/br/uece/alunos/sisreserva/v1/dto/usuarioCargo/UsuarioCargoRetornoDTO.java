2d14779cb5d15ad0f5999d0b22c36e31a5cfd7e2
Andre Lucio Rocha <andrerocha0911@gmail.com>
2025-07-04 09:57:11 -0300

    feat: add documento fiscal para usuario

--- src/main/java/br/uece/alunos/sisreserva/v1/dto/usuarioCargo/UsuarioCargoRetornoDTO.java
+++ src/main/java/br/uece/alunos/sisreserva/v1/dto/usuarioCargo/UsuarioCargoRetornoDTO.java
@@ -2,7 +2,7 @@ package br.uece.alunos.sisreserva.v1.dto.usuarioCargo;
 
 import br.uece.alunos.sisreserva.v1.domain.usuarioCargo.UsuarioCargo;
 
-public record UsuarioCargoRetornoDTO(String cargoId, String cargoNome, String UsuarioId) {
+public record UsuarioCargoRetornoDTO(String cargoId, String cargoNome, String usuarioId) {
     public UsuarioCargoRetornoDTO(UsuarioCargo data) {
         this(data.getCargo().getId(), data.getCargo().getNome(), data.getUsuario().getId());
     }
