a9f2224cfad1172ccbbe15a1acd9cb33c63166c4
Andre Lucio Rocha <andrerocha0911@gmail.com>
2025-07-18 15:50:34 -0300

    feat: removendo not null de tombamento

---
+++ src/main/java/br/uece/alunos/sisreserva/v1/domain/equipamentoEspaco/useCase/CriarEquipamentoEspaco.java
@@ -0,0 +1,9 @@
+package br.uece.alunos.sisreserva.v1.domain.equipamentoEspaco.useCase;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class CriarEquipamentoEspaco {
+
+
+}
---
+++ src/main/java/br/uece/alunos/sisreserva/v1/domain/tipoEquipamento/useCase/CriarTipoEquipamento.java
@@ -0,0 +1,9 @@
+package br.uece.alunos.sisreserva.v1.domain.tipoEquipamento.useCase;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CriarTipoEquipamento {
+
+}
