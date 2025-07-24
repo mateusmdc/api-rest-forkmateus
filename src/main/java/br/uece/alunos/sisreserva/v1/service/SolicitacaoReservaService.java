5823c10874d58ce30f08386a922fe181363e7991
Mateus Matos <38740703+mateusmdc@users.noreply.github.com>
2025-07-24 15:28:34 -0300

    feat: implementado service e service implementation para solicitação de reserva.

---
+++ src/main/java/br/uece/alunos/sisreserva/v1/service/SolicitacaoReservaService.java
@@ -0,0 +1,22 @@
+package br.uece.alunos.sisreserva.v1.service;
+
+import br.uece.alunos.sisreserva.v1.dto.solicitacaoReserva.SolicitacaoReservaDTO;
+import br.uece.alunos.sisreserva.v1.dto.solicitacaoReserva.SolicitacaoReservaRetornoDTO;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
+
+public interface SolicitacaoReservaService {
+    SolicitacaoReservaRetornoDTO criarSolicitacaoReserva(SolicitacaoReservaDTO data);
+    Page<SolicitacaoReservaRetornoDTO> obterSolicitacaoReserva(
+        Pageable pageable,
+        String id,
+        LocalDate dataInicio,
+        LocalDate dataFim,
+        String espacoId,
+        String usuarioSolicitanteId,
+        Integer status,
+        String projetoId
+    );
+}
